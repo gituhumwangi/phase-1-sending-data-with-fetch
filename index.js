b8540ec47368cae5c08b54fe91987332d017c018
dennis <dennis.mwangi@student.moringaschool.com>
2023-06-28 11:17:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+        const formData = {
+                name: "Steve",
+                email: "steve@steve.com"
+        } 
+
+        const configObject = {
+                method: "POST",
+                headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                },
+                body: JSON.stringify(formData)
+        }
+        return fetch("http://localhost:3000/users", configObject)
+
+        .then(res => res.json)
+        .then(object => console.log(object))
+        .catch(error => {
+                alert("Bad things, have happened.")
+                console.log(error)
+        })
+        
+}
